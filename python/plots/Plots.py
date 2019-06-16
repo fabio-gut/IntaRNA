570c24d28b8a34826eac07c93a013ab1387cd61a
fabio-gut <fabio.gutmann@web.de>
2019-06-16 18:59:48 +0200

    reworked plot generation

--- python/plots/Plots.py
+++ python/plots/Plots.py
@@ -9,46 +9,56 @@ from IntaRNApvalue import IntaRNApvalue
 
 
 class Plots:
-    def __init__(self):
-        self.intarna = IntaRNApvalue()
-
-    def get_dist_function(self) -> None:
+    @staticmethod
+    def get_dist_function(query: str, target: str, n: int, shuffle_mode='b') -> None:
         """Gets a distribution function as bar histogram to a target/query sequence combination"""
-        scores = self.intarna.get_scores()
+        i = IntaRNApvalue(['-q', query, '-t', target, '-a', str(n), '-sm', shuffle_mode, '--threads', '0'])
+        scores, non_interactions = i.get_scores()
+        percent_non_int = round(non_interactions / (n + non_interactions) * 100, 1)
+        annot_non_int = '{}% of all sequence pairs had no interaction'.format(percent_non_int)
 
         fig, ax = plt.subplots()
+        ax.annotate(annot_non_int, (0.05, 0.01), rotation=90, size=10)
         n, bins, patches = ax.hist(scores, 100, density=True, facecolor='g', range=(min(scores), 0))
         plt.xlabel('MFE')
-        plt.ylabel('Wahrscheinlichkeit')
-        plt.title('Wahrscheinlichkeit eines IntaRNA scores aus randomisierten Sequenzen')
+        plt.ylabel('MFE Frequency')
+        plt.title('Distribution of IntaRNA scores from shuffled sequences')
         plt.grid(True)
 
         # Try to fit a gaussian distribution
         avg = np.mean(scores)  # average
         var = np.var(scores)  # variance
         std_dev = np.sqrt(var)  # standard deviation
-        # gauss_x = np.linspace(np.min(scores), np.max(scores), 100)  # even spaced numbers over interval
-        gauss_x = np.linspace(np.min(scores), 0, 100)  # even spaced numbers over interval
+
+        gauss_x = np.linspace(np.min(scores), np.max(scores), 100)  # even spaced numbers over interval
         gauss_y = 1.0 / np.sqrt(2 * np.pi * var) * np.exp(-0.5 * ((gauss_x - avg) ** 2 / var))
         # f(x, mu, std_dev) = 1/std_dev*sqrt(2*pi) * e^(-0.5 * ((x-mu)/std_dev)^2)
-        plt.plot(gauss_x, gauss_y, 'k--')
+        plt.plot(gauss_x, gauss_y, 'k--', label='Gauss distribution')
 
+        # TODO: Gumbel distribution
+
+        plt.legend(loc='upper left')
         plt.show()
 
-    def get_pvalue_graph(self, query: str, target: str, max_exp: int = 4) -> None:
-        """Plots the p-value for different shuffle types and amounts, max_exp defines amount with 10^max_exp"""
-        self.intarna.query = query
-        self.intarna.target = target
-        for n in range(1, max_exp):
-            self.intarna.shuffle_query, self.intarna.shuffle_target = True, False
-            plt.plot(10 ** n, self.intarna.calculate_pvalue_empirical(), 'rx')
-            self.shuffle_query, self.shuffle_target = False, True
-            plt.plot(10 ** n, self.intarna.calculate_pvalue_empirical(), 'bx')
-            self.intarna.shuffle_query, self.intarna.shuffle_target = True, True
-            plt.plot(10 ** n, self.intarna.calculate_pvalue_empirical(), 'gx')
+    @staticmethod
+    def get_pvalue_graph(query: str, target: str, max_exp: int = 4) -> None:
+        """Plots the p-value for different shuffle types and # of scores, max_exp defines # scores with 10^max_exp
+        For higher order of exponents this will take a very long time"""
+
+        for n in range(1, max_exp + 1):
+            i = IntaRNApvalue(['-q', query, '-t', target, '--amount', str(10**n), '--shuffle', 'q', '--threads', '0'])
+            plt.semilogx(10 ** n, i.calculate_pvalue_gauss(), 'rx', label='only query shuffled' if n == 1 else '')
+
+            i = IntaRNApvalue(['-q', query, '-t', target, '--amount', str(10**n), '--shuffle', 't', '--threads', '0'])
+            plt.semilogx(10 ** n, i.calculate_pvalue_gauss(), 'bx', label='only target shuffled' if n == 1 else '')
+
+            i = IntaRNApvalue(['-q', query, '-t', target, '--amount', str(10**n), '--shuffle', 'b', '--threads', '0'])
+            plt.semilogx(10 ** n, i.calculate_pvalue_gauss(), 'gx', label='query & target shuffled' if n == 1 else '')
+
         plt.title('p-values for different shuffle types and shuffle amount')
-        plt.xlabel('# of shuffles')
+        plt.xlabel('# of used scores')
         plt.ylabel('p-value')
+        plt.legend(loc='upper left')
         plt.show()
 
 
@@ -57,6 +67,5 @@ if __name__ == '__main__':
     t = 'UUUAAAUUAAAAAAUCAUAGAAAAAGUAUCGUUUGAUACUUGUGAUUAUACUCAGUUAUACAGUAUCUUAAGGUGUUAUUAAUAGUGGUG' \
         'AGGAGAAUUUAUGAAGCUUUUCAAAAGCUUGCUUGUGGCACCUGCAACUCUUGGUCUUUUAGCACCAAUGACCGCUACUGCUAAU'
 
-    p = Plots()
-    p.get_dist_function()
-    # p.get_pvalue_graph(q, t, 4)
+    # Plots.get_dist_function(q, t, 100, 'b')
+    Plots.get_pvalue_graph(q, t, 4)
