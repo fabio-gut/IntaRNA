567b88e1a3ee4c4d2241a2ae51249bd481f8eacb
fabio-gut <fabio.gutmann@web.de>
2019-06-16 12:50:43 +0200

    moved doctest to external file

---
+++ python/Test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# Copyright 2019
+# Author: Fabio Gutmann <fabio.gutmann@jupiter.uni-freiburg.de>
+
+import IntaRNApvalue
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod(IntaRNApvalue)
+
